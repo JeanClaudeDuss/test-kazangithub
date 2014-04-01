e2de0682f84bd2b2758b0298669bc063f70fa3fb
pierre.frayer <pierre.frayer@atos.net>
2014-04-01 16:01:12 +0200

    Add the Conversion class, to convert stuff

---
+++ Conversion.java
@@ -0,0 +1,14 @@
+public class Conversion {
+
+	public Conversion(){}
+
+	public String integerToString(int number){
+		return (String)number;
+	}
+
+	public void nothing(){}
+
+	public Float doubleToFloat(Double number){
+		return (Float)number;
+	}
+}

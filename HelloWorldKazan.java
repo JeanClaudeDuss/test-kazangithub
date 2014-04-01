041ffd658dcbbc7ed0082e1e47a5bb2d141e97cd
pierre.frayer <pierre.frayer@atos.net>
2014-04-01 14:41:15 +0200

    Add sayHelloTo method, to say hello to someone in particular

--- HelloWorldKazan.java
+++ HelloWorldKazan.java
@@ -7,6 +7,10 @@ public Class HelloWorldKazan {
 		System.out.printlnj("Hello World Kazan !");
 	}	
 
+	public void sayHelloTo(String name){
+		System.out.println("Hello "+name);
+	}
+
 	public static void main(String[] args){
 		HelloWorldKazan hello = new HelloWorldKazan();
 	}

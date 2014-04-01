1ad06de0e350b97842da63bc30e922fb742944af
unknown <a578310@L83635.fr01.awl.atosorigin.net>
2014-04-01 11:46:15 +0200

    First commit, adding HelloWord file to the repo

---
+++ HelloWorldKazan.java
@@ -0,0 +1,13 @@
+public Class HelloWorldKazan {
+	public HelloWorldKazan(){
+		sayHello();
+	}
+
+	public void sayHello(){
+		System.out.printlnj("Hello World Kazan !");
+	}	
+
+	public static void main(String[] args){
+		HelloWorldKazan hello = new HelloWorldKazan();
+	}
+}
